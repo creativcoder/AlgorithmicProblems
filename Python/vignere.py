b90ce171d77c87d15d60c6241cb5c2df54debf96
Rahul Sharma <rsconceptx@gmail.com>
2015-05-23 22:45:48 +0530

    added vignere.py

---
+++ Python/vignere.py
@@ -0,0 +1,16 @@
+def rotate(st):
+	a=list(st)
+	temp=a.pop()
+	return "".join(list(temp)+a)
+
+a="abcdefghijklmnopqrstuvwxyz"
+container=[]
+container.append(list(a))
+for i in range(26):
+	a=rotate(a)
+	container.append(list(a))
+
+for i in container:
+	for j in i:
+		print j,
+	print ""
