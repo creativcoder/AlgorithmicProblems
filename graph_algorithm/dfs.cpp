e263636af9f7f406418e62473166e2ad2b8bb7e5
Rahul Sharma <rsconceptx@gmail.com>
2015-09-17 00:44:42 +0530

    Update dfs.cpp

--- graph_algorithm/dfs.cpp
+++ graph_algorithm/dfs.cpp
@@ -24,15 +24,16 @@ class Graph {
     void add_nodes(){
         int data;
         for(int i=0;i<N;i++)
-        cout<<"Enter node data"<<i<<endl;
+        {cout<<"Enter node data"<<i<<endl;
         cin>>data;
 
-        G.push_back(Node(data));
-    }
-    void add_edge(int id,Node which)
-    {
+        G.push_back(Node(data));}
 
-        G.push_back(which);
+    }
+    void add_edge(int src,int dst)
+    {
+        G[src].adj.push_back(G[dst]);
+        cout<<"Added successfully node: "<<G[src].adj[0].data;
     }
 };
 
@@ -41,5 +42,11 @@ int main()
 {
     Graph my_graph(5);
     my_graph.add_nodes();
+
+    my_graph.add_edge(0,3);
+    my_graph.add_edge(2,4);
+    my_graph.add_edge(1,4);
+    my_graph.add_edge(2,3);
+
     return 0;
 }
