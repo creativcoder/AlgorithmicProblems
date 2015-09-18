cca149ad8a929c9465a3998dadb8e02ce8f38740
Rahul Sharma <rsconceptx@gmail.com>
2015-09-18 11:31:49 +0530

    updated to a more simple code

--- C/progress.c
+++ C/progress.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-   int i=0,j;
-   char elem[]={'|','/','--','\\'};
-   while(1)
-   {
-       printf("%c\r",elem[i++]);
-       i%=4;
-       for(j=0;j<40000000;j++);
-   }
-    return 0;
+	int i=0,j;
+	char ar[5] = {'/', '-', '\\', '|'};
+	while(true)
+	{
+		for(j=0;j<23000;j++);
+		printf ("%c\r", ar[i%7]);	
+		i++;
+	}
+	return 0;
 }
