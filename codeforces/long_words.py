628b6a01fb7b81ced814e20aaecfadbaee10c8d7
Rahul Sharma <rsconceptx@gmail.com>
2015-09-18 18:40:59 +0530

    Create long_words.py

---
+++ codeforces/long_words.py
@@ -0,0 +1,13 @@
+http://codeforces.com/problemset/problem/71/A
+
+T = int(raw_input())
+
+while(not T == 0):
+	word = str(raw_input())
+	
+	if len(word)>10:
+		print word[0]+str(len(word[1:len(word)-1]))+word[len(word)-1]
+	else:
+		print word
+		
+	T-=1
