1dc7d75ec6246e52a68bb6cfe1a5c3030680179d
Rahul Sharma <rsconceptx@gmail.com>
2015-09-18 16:34:22 +0530

    Update cryptanalysis.py

--- Python/cryptanalysis.py
+++ Python/cryptanalysis.py
@@ -2,12 +2,21 @@
 
 // Problem : what is cryptanalysis ?
 
-freq_list = [0 for i in range(0,27)]
+freq_list = [0 for i in range(0,256)]
 
-print freq_list
+
+#print freq_list
 
 def freq_table():
 	with open('data.txt', 'r') as f:
-		line = f.readline()
+		line = f.readline().rstrip('\n')
 		for c in line:
-			freq_list[ord(c)%26]+ = 1
+
+			freq_list[ord(c)] += 1
+
+freq_table()
+
+# freq_list.sort()
+
+for i in range(97,123):
+	print chr(i)," ",freq_list[i]
