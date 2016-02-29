c874b0e91788808736b569f524e52cbf68635bc7
Rahul Sharma <rsconceptx@gmail.com>
2016-02-29 22:14:14 +0530

    some refactoring

--- Python/huffman.py
+++ Python/huffman.py
@@ -1,11 +1,22 @@
+class Tree(object):
+    def __init__(self,data,freq):
+        self.data = data
+        self.freq = freq
+
+class Forest(object):
+    def __init__(self,nodes):
+        self.nodes = nodes
+    def sort(self):
+        pass
+    def bump(self):
+        pass
+
 class HuffmanCoder(object):
-    
     def __init__(self,freq_list):
         self.master_table = []
         self.freq_list = []
         self.sorted_freq_list = freq_list
-        
-
+    
     def desc(self):
         self.sorted_freq_list = sorted(self.freq_list,key=lambda freq_list:freq_list[1])
         self.sorted_freq_list.reverse()
@@ -20,10 +31,15 @@ class HuffmanCoder(object):
         self.desc()
         return self.sorted_freq_list
 
-
-freq_list = [['A',16,-1],['B',7,-1],['C',6,-1],['D',6,-1],['E',5,-1]]
-
-letters = ['A','B','C','D','E','F']
-
-huff = HuffmanCoder(freq_list)
-print huff.assign_code()
+def test_case():
+    freq_list = {
+        'a':13,
+        'b':16,
+        'c':2,
+        'd':7,
+        'e':9,
+        'f':3
+        }
+    
+if __name__=='__main__':
+    test_case()
