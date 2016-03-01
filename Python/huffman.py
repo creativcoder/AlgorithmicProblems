70a71445c836fd149b6bbbdcd833d9dce2b61925
Rahul Sharma <rsconceptx@gmail.com>
2016-03-01 15:13:36 +0530

    adding subtree creation method

--- Python/huffman.py
+++ Python/huffman.py
@@ -2,44 +2,50 @@ class Tree(object):
     def __init__(self,data,freq):
         self.data = data
         self.freq = freq
+        self.left = None
+        self.right = None
+        self.encoding = None
+
+    def __str__(self):
+        return "   ({}{})\n/\t\\\n{}\t{}\n".format(self.data,self.freq,self.left,self.right)
 
 class Forest(object):
-    def __init__(self,nodes):
-        self.nodes = nodes
-    def sort(self):
-        pass
-    def bump(self):
-        pass
-
-class HuffmanCoder(object):
     def __init__(self,freq_list):
-        self.master_table = []
-        self.freq_list = []
-        self.sorted_freq_list = freq_list
-    
-    def desc(self):
-        self.sorted_freq_list = sorted(self.freq_list,key=lambda freq_list:freq_list[1])
-        self.sorted_freq_list.reverse()
+        self.nodes = []
+        for i in freq_list.keys():
+            self.nodes.append(Tree(i,freq_list.get(i)))
 
-    def assign_code(self):
-        last_two = []
-        last_two.append(self.sorted_freq_list[-2][0]+self.sorted_freq_list[-1][0])
-        last_two.append(self.sorted_freq_list[-2][1]+self.sorted_freq_list[-1][1])
-        self.sorted_freq_list.pop()
-        self.sorted_freq_list.pop()
-        self.sorted_freq_list.append(last_two)
-        self.desc()
-        return self.sorted_freq_list
+    def sort_nodes(self):
+        self.nodes = sorted(self.nodes,key=lambda x:(x.freq))
+
+    def make_trees(self):
+        while(len(self.nodes) is not 1):
+            temp_node_data = self.nodes[0].data + self.nodes[1].data
+            temp_node_freq = self.nodes[0].freq + self.nodes[1].freq
+            temp_node = Tree(temp_node_data,temp_node_freq)
+            temp_node.left = self.nodes[0]
+            temp_node.right = self.nodes[1]
+            temp_node.left.encoding = 0
+            temp_node.left.encoding = 1
+            self.nodes = self.nodes[2:]
+            self.nodes.append(temp_node)
+            self.sort_nodes()
+
+    def assign_codeword(self):
+        pass
 
 def test_case():
     freq_list = {
-        'a':13,
-        'b':16,
-        'c':2,
-        'd':7,
-        'e':9,
-        'f':3
-        }
-    
+    'a':13,
+    'b':8,
+    'c':8,
+    'd':3
+    }
+
+    forest = Forest(freq_list)
+    forest.sort_nodes()
+    forest.make_trees()
+    # forest.assign_codeword()
+
 if __name__=='__main__':
     test_case()
